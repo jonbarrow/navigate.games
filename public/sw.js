5b6ef79714ed011a3aa18eb823e6b5368de19503
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-06-13 19:02:17 -0400

    chore: make service worker network-first

--- public/sw.js
+++ public/sw.js
@@ -35,25 +35,22 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches.match(event.request).then((response) => {
-			if (response) {
-				return response;
-			}
-
-			const fetchRequest = event.request.clone();
-
-			return fetch(fetchRequest).then((response) => {
-				if (!response || response.status !== 200 || response.type !== 'basic') {
-					return response;
-				}
-
+		// * Try network first, always
+		fetch(event.request.clone()).then((response) => {
+			if (response && response.status === 200 && response.type === 'basic') {
 				caches.open(CACHE_NAME).then((cache) => {
 					cache.put(event.request, response.clone());
 				});
+			}
 
-				return response;
-			}).catch((e) => {
-				// * This should never happen, but handle it anyway
+			return response;
+		}).catch((e) => {
+			// * Network failed, assume offline and try cache if possible
+			return caches.match(event.request).then((cachedResponse) => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
+
 				if (event.request.destination === 'document') {
 					return new Response(
 						`<html>
